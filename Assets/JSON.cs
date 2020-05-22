9951042bb74dca050aabecfb34666ecdd18099e4
Ryo KUNO <kuno@aise.ics.saitama-u.ac.jp>
2020-05-22 12:55:52 +0900

    first commit

---
+++ Assets/JSON.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[Serializable]
+public class MyClass
+{
+    //以下JSONの中の要素
+    public int level;
+    public float timeElapsed;
+    public string playerName;
+}
+
+public class JSON : MonoBehaviour
+{
+    MyClass myObject = new MyClass();
+    public GameObject test_text = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        myObject.level = 10;
+        myObject.timeElapsed = 50.0f;
+        myObject.playerName = "Player1";
+
+        
+
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            string json = JsonUtility.ToJson(myObject);
+            Text test = test_text.GetComponent<Text>();
+            test.text = json;
+        }    
+    }
+
+
+}
